b0e3b4fbec85265e56be1540bd6d3fabbaddd5d9
thiyagu <thiyaga0513@gmail.com>
2024-10-19 16:55:48 +0530

    added capacitor in config.json

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Food-ordering',
+  webDir: 'www'
+};
+
+export default config;
